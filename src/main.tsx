53c9c26204d22285a03b42eb4a7a038011f3fbdf
Roman <roman@topluxrealty.com>
2024-04-09 21:00:18 +0300

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+// this manifest is used temporarily for development purposes
+const manifestUrl = 'https://seatovod100500.github.io/counter-twa/tonconnect-manifest.json';
+
+
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
-  </React.StrictMode>,
+  </TonConnectUIProvider>,
 )
